a8edebe6b26c81c380baf4b990175ae1322a0c1d
SeleniumProject <seleniumden@gmail.com>
2020-12-20 01:27:28 +0530

    Test

--- seleniumjavaframework/src/test/java/com/demowebshop/pages/LoginPage.java
+++ seleniumjavaframework/src/test/java/com/demowebshop/pages/LoginPage.java
@@ -17,7 +17,6 @@ public class LoginPage {
 		System.out.println("testing message");
 		System.out.println("testing message");
 		System.out.println("testing message");
-		System.out.println("testing message");
 	}
 	
 	// Page Object Mechanism
